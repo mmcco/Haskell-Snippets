320f0409cc518e9656c718e33ce0e408ad92d9cf
Michael McConville <mmcconville@colgate.edu>
2013-06-03 13:24:47 -0400

    updating functionality of get_addresses.hs, accounting for short lines

--- get_addresses.hs
+++ get_addresses.hs
@@ -4,11 +4,17 @@ import qualified Data.List as List
 
 getAddresses :: String -> [String]
 getAddresses x = List.nub .
-                 filter ((/=) "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") . 
-                 map (flip (!!) 2) . 
-                 map words . 
+                 filter ((/=) "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") .
+                 map (flip (!!) 2) .
+                 filter (\x -> (>=) (length x) 3) .
+                 map words .
                  drop 3 .
                  lines $ x
+
+shortLines :: String -> [(Int, [String])]
+shortLines x = filter (\x -> (<) (length (snd x)) 3) .
+               (\xs -> zip [1..length xs] (map words xs)) .
+               lines $ x
 
 main = do
    transactions <- getContents
