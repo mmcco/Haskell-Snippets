0e78c055b3f721b8d36bb5d42039fecf1985ee1f
Michael McConville <momcconville@gmail.com>
2013-06-18 21:56:04 +0400

    fixing category parse, minor hlint changes

--- genlinks.hs
+++ genlinks.hs
@@ -1,11 +1,15 @@
 import System.Directory (getDirectoryContents)
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf, isPrefixOf)
+
+getCategory :: [String] -> String
+getCategory [] = "Misc"
+getCategory xs = (\x -> if length x < 2 then "Misc" else x !! 1) . words . head $ xs
 
 main = do
           dirContents <- getDirectoryContents "/home/mike/jekyll"
           let markdowns = map ("/home/mike/jekyll/" ++ ) . filter (isSuffixOf ".md") $ dirContents
-          files <- sequence . map readFile $ markdowns
-          let categories = map ((flip (!!)) 1 . filter (\x -> head x == "category:") . map words . takeWhile ((/=) "---") . drop 1 . dropWhile ((/=) "---") . lines) files
-              --parseMe = zip markdowns categories
+          files <- mapM readFile markdowns
+          let categories = map (getCategory . filter (isPrefixOf "categories:") . takeWhile ((/=) "---") . drop 1 . dropWhile ((/=) "---") . lines) files
+              parseMe = zip markdowns categories
           --print . unlines . map (\x -> fst x ++ " " ++ snd x) $ parseMe
-          print "compiled"
+          print . unwords $ categories
