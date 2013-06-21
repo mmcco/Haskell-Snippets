6426b8751a815f1f665d74e2c9f4ee425df71aa3
plsql <master@branch>
2013-06-21 19:29:51 -0400

    better version of genlinks.hs

--- genlinks.hs
+++ genlinks.hs
@@ -1,15 +1,17 @@
 import System.Directory (getDirectoryContents)
 import Data.List (isSuffixOf, isPrefixOf)
 
-getCategory :: [String] -> String
-getCategory [] = "Misc"
-getCategory xs = (\x -> if length x < 2 then "Misc" else x !! 1) . words . head $ xs
+getCategory :: String -> String
+getCategory file = if length (words headerLine) < 2 then "Misc." else (flip (!!)) 2 . words $ headerLine
+    where headerLine = head . filter (isPrefixOf "category:") . lines $ file
+
+getTitle :: String -> String
+getTitle file = if length (words headerLine) < 2 then "Untitled" else (flip (!!)) 2 . words $ headerLine
+    where headerLine = head . filter (isPrefixOf "title:") . lines $ file
 
 main = do
           dirContents <- getDirectoryContents "/home/mike/jekyll"
           let markdowns = map ("/home/mike/jekyll/" ++ ) . filter (isSuffixOf ".md") $ dirContents
           files <- mapM readFile markdowns
-          let categories = map (getCategory . filter (isPrefixOf "categories:") . takeWhile ((/=) "---") . drop 1 . dropWhile ((/=) "---") . lines) files
-              parseMe = zip markdowns categories
-          --print . unlines . map (\x -> fst x ++ " " ++ snd x) $ parseMe
-          print . unwords $ categories
+          let tuples = map (\x -> (getTitle x, getCategory x)) files
+          print tuples
