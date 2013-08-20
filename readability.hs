5b227c2a6e1b324275a3dd853635a401f6a2289d
Michael McConville <momcconville@gmail.com>
2013-08-20 13:01:18 -0400

    removing header comments

--- readability.hs
+++ readability.hs
@@ -1,12 +1,3 @@
--- have to deal with ellipses
--- try splitting into words first
--- there is a problem in the fact that "i.e." and "e.g." are split in half by breakNext
--- the resulting "e." and "i." are common and cannot be pattern-matched
--- the above problem is tentatively fixed
--- however, it's splitting senteces in quotations before the closing quotation mark
--- I'm going to try to fix this by first breaking by word
-
-
 import Data.List (isPrefixOf, isSuffixOf, intersect)
 import Control.Applicative
 import System.Environment (getArgs)
@@ -17,7 +8,8 @@ sentences :: String -> [String]
 sentences = filter (\x -> length x > 1) . map (trim " \n\t") . combine . words
     where combine = foldl (\acc sent -> if isBreak (mlast acc) && not (hasException (mlast acc) sent)
                                           then acc ++ [sent]
-                                          else safeInit acc ++ [mlast acc ++  " " ++ sent]) []
+                                          else safeInit acc ++ [mlast acc ++  " " ++ sent])
+                        []
           hasException xs ys = or $ isSuffixOf <$> ["Mr.", "Mrs.", "Dr.", "St.", "cf.", "eg.", "i.e.", "e.g."] <*> [xs]
           isBreak xs = or $ isSuffixOf <$> [".", "!", "?", ".\"",".'","!\"","!'","?\"","?'"] <*> [xs]
 
